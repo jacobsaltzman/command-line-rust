f4085984fdc4c157a90dd698dbcdb389ba05784a
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2024-07-16 16:35:56 -0700

    Adds trueok test

---
+++ hello/target/debug/deps/cli-f04670c1c60e824f.d
@@ -0,0 +1,5 @@
+/home/ickus/Development/code/command-line-rust/hello/target/debug/deps/cli-f04670c1c60e824f: tests/cli.rs
+
+/home/ickus/Development/code/command-line-rust/hello/target/debug/deps/cli-f04670c1c60e824f.d: tests/cli.rs
+
+tests/cli.rs:
---
+++ hello/target/debug/deps/true-507f65547ca04151.d
@@ -0,0 +1,5 @@
+/home/ickus/Development/code/command-line-rust/hello/target/debug/deps/true-507f65547ca04151: src/bin/true.rs
+
+/home/ickus/Development/code/command-line-rust/hello/target/debug/deps/true-507f65547ca04151.d: src/bin/true.rs
+
+src/bin/true.rs:
