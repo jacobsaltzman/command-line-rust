da7b86031de89e642bfcde33889c861ca0aeca2d
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2024-07-16 16:27:28 -0700

    Test deps

---
+++ hello/target/debug/deps/difflib-483f9fac2d3123c2.d
@@ -0,0 +1,10 @@
+/home/ickus/Development/code/command-line-rust/hello/target/debug/deps/libdifflib-483f9fac2d3123c2.rmeta: /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/difflib-0.4.0/src/lib.rs /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/difflib-0.4.0/src/differ.rs /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/difflib-0.4.0/src/sequencematcher.rs /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/difflib-0.4.0/src/utils.rs
+
+/home/ickus/Development/code/command-line-rust/hello/target/debug/deps/libdifflib-483f9fac2d3123c2.rlib: /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/difflib-0.4.0/src/lib.rs /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/difflib-0.4.0/src/differ.rs /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/difflib-0.4.0/src/sequencematcher.rs /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/difflib-0.4.0/src/utils.rs
+
+/home/ickus/Development/code/command-line-rust/hello/target/debug/deps/difflib-483f9fac2d3123c2.d: /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/difflib-0.4.0/src/lib.rs /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/difflib-0.4.0/src/differ.rs /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/difflib-0.4.0/src/sequencematcher.rs /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/difflib-0.4.0/src/utils.rs
+
+/home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/difflib-0.4.0/src/lib.rs:
+/home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/difflib-0.4.0/src/differ.rs:
+/home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/difflib-0.4.0/src/sequencematcher.rs:
+/home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/difflib-0.4.0/src/utils.rs:
---
+++ hello/target/debug/deps/lazy_static-065126ceef935bda.d
@@ -0,0 +1,8 @@
+/home/ickus/Development/code/command-line-rust/hello/target/debug/deps/liblazy_static-065126ceef935bda.rmeta: /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/lib.rs /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/inline_lazy.rs
+
+/home/ickus/Development/code/command-line-rust/hello/target/debug/deps/liblazy_static-065126ceef935bda.rlib: /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/lib.rs /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/inline_lazy.rs
+
+/home/ickus/Development/code/command-line-rust/hello/target/debug/deps/lazy_static-065126ceef935bda.d: /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/lib.rs /home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/inline_lazy.rs
+
+/home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/lib.rs:
+/home/ickus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/inline_lazy.rs:
