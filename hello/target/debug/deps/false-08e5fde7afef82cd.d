3ab99c9ff1367a5de32d447601b7a5576984ca54
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2024-07-16 16:40:01 -0700

    Adds false abort test and bin

---
+++ hello/target/debug/deps/false-08e5fde7afef82cd.d
@@ -0,0 +1,5 @@
+/home/ickus/Development/code/command-line-rust/hello/target/debug/deps/false-08e5fde7afef82cd: src/bin/false.rs
+
+/home/ickus/Development/code/command-line-rust/hello/target/debug/deps/false-08e5fde7afef82cd.d: src/bin/false.rs
+
+src/bin/false.rs:
---
+++ hello/target/debug/false.d
@@ -0,0 +1,1 @@
+/home/ickus/Development/code/command-line-rust/hello/target/debug/false: /home/ickus/Development/code/command-line-rust/hello/src/bin/false.rs
